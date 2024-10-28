47389989590d42788393f6bf7dc9c0387a82b049
hoon-hoon <kc0393@naver.com>
2024-10-28 16:52:26 +0900

    feat: API 데이터 처리 수정, 레이아웃 개선 및 별점 기능 추가

--- src/pages/modal/BookModal.tsx
+++ src/pages/modal/BookModal.tsx
@@ -13,7 +13,7 @@ const BookModal = () => {
   const navigate = useNavigate();
   const bookmarkRef = useRef<HTMLDivElement>(null);
 
-  const bookId = '173930433'; // 임시 책 Id
+  const bookId = '2173713'; // 임시 책 Id
   const backgroundColor = 'rgba(36, 56, 104, 0.5)'; // 임시 배경색 (투명도 50)
 
   useEffect(() => {
@@ -21,6 +21,7 @@ const BookModal = () => {
       try {
         const data = await getBookDataById(bookId);
         setBook(data);
+        console.log(data);
       } catch (error) {
         console.error('Failed to fetch book details:', error);
       } finally {
@@ -52,10 +53,30 @@ const BookModal = () => {
 
   if (!book) return <p>Loading...</p>;
 
-  const [leftTitle, rightTitle] = book.title.split(' - ');
   const author = book.author.split(' (')[0];
   const formatCategory = (category: string) =>
-    category.split(' > ').map(item => `#${item.replace(/\s+/g, '_')}`);
+    category
+      .split('/')
+      .map(item => `#${item.trim()}`)
+      .join(' ');
+
+  const renderRating = (rating: number) => {
+    const maxRating = 5;
+    const filledHearts = Math.round(rating / 2);
+
+    return (
+      <div className='flex space-x-0.5'>
+        {Array.from({ length: maxRating }, (_, index) => (
+          <img
+            key={index}
+            src={`/Icon/${index < filledHearts ? 'heart.svg' : 'white-heart.svg'}`}
+            alt='heart'
+            className='w-6 h-6'
+          />
+        ))}
+      </div>
+    );
+  };
 
   return (
     <div className='fixed inset-0 z-50 bg-white'>
@@ -77,29 +98,27 @@ const BookModal = () => {
         />
 
         <div className='w-[15%] flex items-center justify-center'>
-          <SlidingTitle title={rightTitle} />
+          <SlidingTitle title={book.title} />
         </div>
         <div className='w-[80%] flex items-center justify-center'>
           <div className='relative max-w-5xl w-full h-full p-12 flex flex-col items-center justify-center overflow-y-auto'>
-            <div className='flex flex-col md:flex-row items-center gap-12'>
+            <div className='flex flex-col md:flex-row items-end gap-12'>
               <img
                 className='w-full max-w-md md:max-w-lg h-auto object-cover rounded-lg shadow-xl'
                 src={book.cover}
                 alt={book.title}
               />
-              <div className='flex-1'>
-                <p className='text-h4 mb-4'>{leftTitle}</p>
-                <p className='text-h5 mb-2'>{author}</p>
-                <div className='text-body1 mb-4 space-y-2'>
-                  {formatCategory(book.category_name).map((tag, index) => (
-                    <div key={index}>{tag}</div>
-                  ))}
+              <div className='flex-1 flex flex-col justify-end'>
+                <div>
+                  <p className='text-h4 mb-4'>{book.title}</p>
+                  <p className='text-h5 mb-2'>{author}</p>
+                  <div className='text-body1 mb-4'>{formatCategory(book.category_name)}</div>
+                  <div className='flex items-center mb-6'>
+                    {renderRating(Number(book.rating_info))}
+                    <span className='text-[#DD0000] text-body1 ml-2'>{book.rating_info}</span>
+                  </div>
+                  <p className='text-body1 leading-relaxed'>{book.description}</p>
                 </div>
-
-                <div className='flex items-center mb-6'>
-                  <p> 평점: {book.rating_info}</p>
-                </div>
-                <p className='text-body1 leading-relaxed'>{book.description}</p>
               </div>
             </div>
           </div>
