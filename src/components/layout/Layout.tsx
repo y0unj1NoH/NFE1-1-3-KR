8c26d3a33c4b3e8f7c2cac3bf7a7bfc720326710
KiHoon Roh <started677@gmail.com>
2024-10-30 15:32:44 +0900

    fix: rebase 충돌 파일 추가

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,22 @@
+import { Outlet } from 'react-router-dom';
+
+import { Header } from './Header';
+
+import { useBackgroundColorState } from 'context';
+
+export const Layout = () => {
+  const { backgroundColor } = useBackgroundColorState();
+
+  return (
+    <>
+      <Header />
+      <main
+        className={`mx-auto pt-[6rem] w-full min-h-[calc(100vh-6rem)] ${backgroundColor} transition-all`}
+      >
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Layout;
