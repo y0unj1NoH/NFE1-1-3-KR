5b55b0f9750e0e2dcc0833f5cd04d75c268817c0
he2e2 <dddd6368@naver.com>
2024-10-30 11:12:00 +0900

    feat: 문구 애니메이션 추가

--- src/pages/home/Intro.tsx
+++ src/pages/home/Intro.tsx
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import gsap from 'gsap';
+import { useRef, useEffect } from 'react';
 
 import { Background } from 'components';
 import { useIntersectionObserver, useSetBackgroundColor } from 'hooks';
@@ -6,15 +7,39 @@ import { useIntersectionObserver, useSetBackgroundColor } from 'hooks';
 export const Intro = () => {
   const pageRef = useRef<HTMLDivElement>(null);
   const isVisible = useIntersectionObserver(pageRef);
+  const paragraphsRef = useRef<HTMLParagraphElement[]>([]);
 
   useSetBackgroundColor('bg-primary', isVisible);
+
+  useEffect(() => {
+    if (isVisible) {
+      gsap.fromTo(
+        paragraphsRef.current,
+        { opacity: 0, y: 20 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.8,
+          stagger: 0.4,
+        },
+      );
+    }
+  }, [isVisible]);
 
   return (
     <div className='h-[calc(100vh-6rem)] w-full relative flex items-center p-[8rem]' ref={pageRef}>
       <div className='text-white text-[4rem] translate-y-[-3rem] flex flex-col gap-4'>
-        <p className='translate-x-[2rem]'>DISCOVER</p>
-        <p className='translate-x-[6rem]'>KOREAN STORIES</p>
-        <p>THAT TOUCH THE SOUL</p>
+        {['DISCOVER', 'KOREAN STORIES', 'THAT TOUCH THE SOUL'].map((text, index) => (
+          <p
+            className={index === 0 ? 'translate-x-[2rem]' : index === 1 ? 'translate-x-[6rem]' : ''}
+            key={index}
+            ref={el => {
+              if (el) paragraphsRef.current[index] = el;
+            }}
+          >
+            {text}
+          </p>
+        ))}
       </div>
       {isVisible && <Background />}
     </div>
