7f40ba711fe7c98bb8add43b9049b54bb187b9ad
he2e2 <dddd6368@naver.com>
2024-10-30 12:10:57 +0900

    feat: 구름 애니메이션 추가

--- src/pages/home/Intro.tsx
+++ src/pages/home/Intro.tsx
@@ -11,6 +11,9 @@ export const Intro = () => {
   const paragraphsRef = useRef<HTMLParagraphElement[]>([]);
   const setIsVisible = useIntroStore(state => state.setIsVisible);
 
+  const cloud1Ref = useRef(null);
+  const cloud3Ref = useRef(null);
+
   useEffect(() => {
     setIsVisible(isVisible);
   }, [isVisible, setIsVisible]);
@@ -32,11 +35,45 @@ export const Intro = () => {
     }
   }, [isVisible]);
 
+  useEffect(() => {
+    const animateClouds = () => {
+      gsap.to(cloud1Ref.current, {
+        y: 10,
+        x: 10,
+        duration: 3,
+        repeat: -1,
+        yoyo: true,
+        ease: 'sine.inOut',
+      });
+
+      gsap.to(cloud3Ref.current, {
+        y: 10,
+        x: -10,
+        duration: 2,
+        repeat: -1,
+        yoyo: true,
+        ease: 'sine.inOut',
+      });
+    };
+
+    animateClouds();
+  }, []);
+
   return (
     <div className='h-[calc(100vh-6rem)] w-full relative flex items-center p-[8rem]' ref={pageRef}>
       <div className='w-[234px] h-[234px] bg-[#c3634d] rounded-full absolute top-0 right-[6rem]' />
-      <img alt='cloud-1' className='absolute right-[3rem] top-[23rem]' src='/bg/cloud-1.svg' />
-      <img alt='cloud-3' className='absolute top-[10rem] right-[14rem]' src='/bg/cloud-3.svg' />
+      <img
+        alt='cloud-1'
+        className='absolute right-[3rem] top-[23rem]'
+        ref={cloud1Ref}
+        src='/bg/cloud-1.svg'
+      />
+      <img
+        alt='cloud-3'
+        className='absolute top-[10rem] right-[14rem]'
+        ref={cloud3Ref}
+        src='/bg/cloud-3.svg'
+      />
       <div className='text-white text-[4rem] translate-y-[-3rem] flex flex-col gap-4'>
         {['DISCOVER', 'KOREAN STORIES', 'THAT TOUCH THE SOUL'].map((text, index) => (
           <p
