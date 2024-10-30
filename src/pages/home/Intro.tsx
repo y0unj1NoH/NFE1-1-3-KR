fc75368daecc03b2649228c5aa99d74ba486cd37
he2e2 <dddd6368@naver.com>
2024-10-30 10:50:39 +0900

    feat: 문구 추가

--- src/pages/home/Intro.tsx
+++ src/pages/home/Intro.tsx
@@ -7,11 +7,15 @@ export const Intro = () => {
   const pageRef = useRef<HTMLDivElement>(null);
   const isVisible = useIntersectionObserver(pageRef);
 
-  useSetBackgroundColor('bg-intro', isVisible);
+  useSetBackgroundColor('bg-primary', isVisible);
 
   return (
-    <div className='h-[calc(100vh-6rem)] w-full relative' ref={pageRef}>
-      <h1>Intro</h1>
+    <div className='h-[calc(100vh-6rem)] w-full relative flex items-center p-[8rem]' ref={pageRef}>
+      <div className='text-white text-[4rem] translate-y-[-3rem] flex flex-col gap-4'>
+        <p className='translate-x-[2rem]'>DISCOVER</p>
+        <p className='translate-x-[6rem]'>KOREAN STORIES</p>
+        <p>THAT TOUCH THE SOUL</p>
+      </div>
       {isVisible && <Background />}
     </div>
   );
