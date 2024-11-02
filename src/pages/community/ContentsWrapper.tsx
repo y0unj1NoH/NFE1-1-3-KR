c9319414763db112cbf989d595b16b0678b06e8b
he2e2 <dddd6368@naver.com>
2024-11-02 18:49:39 +0900

    feat: 화면에 보이는 요소만 렌더링 되도록 애니메이션 및 react-window 설정 추가

--- src/pages/community/ContentsWrapper.tsx
+++ src/pages/community/ContentsWrapper.tsx
@@ -1,16 +1,44 @@
-import { useState } from 'react';
+import gsap from 'gsap';
+import { useState, useRef } from 'react';
+import { FixedSizeList as List, type ListChildComponentProps } from 'react-window';
 
 import { DetailPost } from './DetailPost';
 import { Post } from './Post';
 import { WritePost } from './WritePost';
 
 import 'styles/scroll.css';
-import { usePostList } from 'hooks';
+import { usePostList, useIntersectionObserver } from 'hooks';
 
 export const ContentsWrapper = () => {
   const [selectedPostId, setSelectedPostId] = useState<string | null>(null);
 
   const { data: posts } = usePostList();
+
+  const Row = ({ index, style }: ListChildComponentProps) => {
+    const post = posts?.[index];
+    const ref = useRef<HTMLDivElement>(null);
+    const isIntersecting = useIntersectionObserver(ref, { threshold: 0.1 });
+
+    if (isIntersecting && ref.current) {
+      gsap.to(ref.current, { opacity: 1, y: 0, duration: 0.5, ease: 'power3.out' });
+    } else if (ref.current) {
+      gsap.set(ref.current, { opacity: 0, y: 20 });
+    }
+
+    if (!post) return null;
+
+    return (
+      <div
+        onClick={() => {
+          setSelectedPostId(post.post_id as string);
+        }}
+        ref={ref}
+        style={style}
+      >
+        <Post post={post} />
+      </div>
+    );
+  };
 
   return (
     <div className='w-full h-full p-2'>
@@ -18,17 +46,17 @@ export const ContentsWrapper = () => {
       {selectedPostId != null ? (
         <DetailPost onClose={setSelectedPostId} postId={selectedPostId} />
       ) : (
-        <div className='w-full h-full p-4 overflow-y-scroll'>
-          {posts?.map(post => (
-            <div
-              key={post.post_id}
-              onClick={() => {
-                setSelectedPostId(post.post_id as string);
-              }}
+        <div className='w-full h-full p-4'>
+          {posts ? (
+            <List
+              height={Math.min(650, posts.length * 256)}
+              itemCount={posts.length}
+              itemSize={256}
+              width='100%'
             >
-              <Post post={post} />
-            </div>
-          ))}
+              {Row}
+            </List>
+          ) : null}
         </div>
       )}
     </div>
