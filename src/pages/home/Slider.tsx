8f8e2c4170e70b98550838365a5147a030f8d2fb
y0unj1NoH <nohyoong711@gmail.com>
2024-11-05 01:02:40 +0900

    feat: 슬라이더 아이템 호버 애니메이션 추가

--- src/pages/home/Slider.tsx
+++ src/pages/home/Slider.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
 import SliderItem from './SliderItem';
 
--- src/pages/home/SliderItem.tsx
+++ src/pages/home/SliderItem.tsx
@@ -21,12 +21,12 @@ const SliderItem = ({ src, alt, bookId, onCardClick }: SliderItemProps) => {
 
   return (
     <div
-      className='wheel__card group absolute [perspective:1000px] top-0 left-0 w-[13%] max-w-[300px] aspect-[200/295]'
+      className='wheel__card absolute [perspective:1000px] top-0 left-0 w-[13%] max-w-[300px] aspect-[200/295]'
       data-color={bookColor}
       onClick={() => onCardClick && onCardClick(bookId, bookColor || 'transparent')}
     >
       <div className='faces relative w-full h-full rounded-xl [transform-style:preserve-3d]'>
-        <div className='absolute w-full h-full shadow-xl [backface-visibility:hidden]'>
+        <div className='absolute w-full h-full shadow-xl hover:shadow-2xl [backface-visibility:hidden] transition-transform duration-500 ease-in-out transform hover:translate-y-2'>
           <img
             alt={alt}
             className='w-full h-full object-cover'
