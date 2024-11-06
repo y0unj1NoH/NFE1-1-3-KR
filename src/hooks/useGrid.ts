5129a159e0c3c49138dedc95c9fd2228ee418fe6
y0unj1NoH <nohyoong711@gmail.com>
2024-11-06 15:55:23 +0900

    refactor: useGrid 리팩터링

--- src/hooks/useGrid.ts
+++ src/hooks/useGrid.ts
@@ -1,33 +1,33 @@
 import { gsap } from 'gsap';
 import { Flip } from 'gsap/Flip';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import { useBookModalStore } from 'stores';
 import type { BookData } from 'types';
 import { handleItemClick } from 'utils';
 
 export const useGrid = ({ data }: { data: BookData[] }) => {
-  const gallery = document.querySelector<HTMLDivElement>('.gallery');
-  const modal = document.querySelector<HTMLDivElement>('.modal')!;
-  const { setActiveItem } = useBookModalStore();
+  const galleryRef = useRef<HTMLDivElement>(null);
 
   const [items, setItems] = useState<HTMLDivElement[]>(
     gsap.utils.toArray<HTMLDivElement>('.gallery__item'),
   );
+
+  const { setActiveItem } = useBookModalStore();
 
   useEffect(() => {
     setItems(gsap.utils.toArray<HTMLDivElement>('.gallery__item'));
   }, [data]);
 
   useEffect(() => {
-    if (gallery && items) {
-      gsap.to(gallery, { autoAlpha: 1, duration: 0.2 });
+    if (galleryRef.current && items) {
+      gsap.to(galleryRef.current, { autoAlpha: 1, duration: 0.2 });
       gsap.from(items, { autoAlpha: 0, yPercent: 30, stagger: 0.04 });
     }
-  }, [gallery, items]);
+  }, [galleryRef.current, items]);
 
   useEffect(() => {
-    if (!gallery || !items.length) {
+    if (!galleryRef.current || !items.length) {
       return;
     }
 
@@ -36,8 +36,10 @@ export const useGrid = ({ data }: { data: BookData[] }) => {
     items.map((item: HTMLDivElement, index: number) => {
       item.addEventListener('click', () => {
         setActiveItem({ type: 'grid', index });
-        handleItemClick(item, modal);
+        handleItemClick(item);
       });
     });
   }, [items]);
+
+  return { galleryRef };
 };
--- src/pages/home/Grid.tsx
+++ src/pages/home/Grid.tsx
@@ -4,11 +4,14 @@ import { useGrid } from 'hooks';
 import type { BookData } from 'types';
 
 const Grid = ({ data }: { data: BookData[] }) => {
-  useGrid({ data });
+  const { galleryRef } = useGrid({ data });
 
   return (
     <div className='app relative top-4 w-[70vw] mx-auto h-[calc(100vh-8rem)] overflow-y-auto pb-4'>
-      <div className='gallery grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[3.6%] gap-y-[2%] p-2.5'>
+      <div
+        className='gallery grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[3.6%] gap-y-[2%] p-2.5'
+        ref={galleryRef}
+      >
         {data.map((bookData, index) => {
           return (
             <GridItem
