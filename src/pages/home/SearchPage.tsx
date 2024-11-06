f49c1c816ffa8ac185423e2190c2ef62d8b68c25
y0unj1NoH <nohyoong711@gmail.com>
2024-11-06 15:24:09 +0900

    fix: 인트로에서 스크롤 시 구름, 타이틀 안보이게 처리

--- src/pages/home/SearchPage.tsx
+++ src/pages/home/SearchPage.tsx
@@ -1,21 +1,26 @@
+import { useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 import { MainClouds } from './MainClouds';
 import { SearchableBookList } from './SearchableBookList';
 
-import { useSetBackgroundColor, useCloseModal } from 'hooks';
+import { useIntersectionObserver, useSetBackgroundColor, useCloseModal } from 'hooks';
 import { BookModal } from 'pages';
 import { useBookModalStore } from 'stores';
 
 export const SearchPage = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const isVisible = useIntersectionObserver(containerRef);
+  useSetBackgroundColor('bg-secondary', true);
+
   const { bookModalData } = useBookModalStore();
   const { modalRef, closeModal } = useCloseModal();
   useSetBackgroundColor('bg-secondary', true);
 
   return (
-    <div className='h-[calc(100vh-6rem)] w-full overflow-hidden relative'>
-      <MainClouds />
-      <SearchableBookList />
+    <div className='h-[calc(100vh-6rem)] w-full overflow-hidden relative' ref={containerRef}>
+      {isVisible && <MainClouds />}
+      <SearchableBookList isVisible={isVisible} />
       {ReactDOM.createPortal(
         <div
           className='modal fixed inset-0 opacity-0 pointer-events-none z-[10000]'
